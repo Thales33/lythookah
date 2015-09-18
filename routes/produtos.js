d0c94ac644e1025153f679ae2bef0881541b38e7
Thales <tpsthales@gmail.com>
2015-09-18 16:42:44 -0300

    Produtos ok1

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+//process.env.DATABASE_URL
 
-var conString = process.env.DATABASE_URL;
-// var conString = "postgres://postgres:ankit1234@localhost:5432/nodedb_development";
-var connection = new pg.Client(conString);
-// Connect PostgreSQL
-connection.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-});
+
+
 
 
 router.get('/', function(req, res, next) {
@@ -21,15 +15,19 @@ router.get('/', function(req, res, next) {
  });
 
 router.get('/listprodutos', function(req, res) {
-	connection.query('SELECT * FROM PRODUTOS', function(err, result) { 
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('SELECT * FROM PRODUTOS', function(err, result) { 
+      done();
     if (err){
       console.log(err);
     }
     res.render('produtos/listprodutos', {
-    	produtos: result,
+      produtos: result,
         title: 'Produtos - LYT Hookah'});
+    });
   });
 });
+	
 	/*var results = [];
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       var query = client.query("SELECT * FROM LYTHOOKAH.PRODUTOS ORDER BY ASC;");
@@ -68,8 +66,9 @@ router.post('/add', function(req, res){
                  precoVenda: req.body.precoVenda,
                  tipo: req.body.tipo,
                  marca: req.body.marca }
-
- connection.query('INSERT INTO PRODUTOS (descricao,precoCusto,precoVenda,tipo,marca) VALUES ($1,$2,$3,$4,$5)', [retorno.descricao, retorno.precoCusto, retorno.precoVenda, retorno.tipo, retorno.marca], function(err, result) {
+pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('INSERT INTO PRODUTOS (descricao,precoCusto,precoVenda,tipo,marca) VALUES ($1,$2,$3,$4,$5)', [retorno.descricao, retorno.precoCusto, retorno.precoVenda, retorno.tipo, retorno.marca], function(err, result) {
+    done();
     if (err){
       console.log(err);
     }
@@ -81,8 +80,10 @@ router.post('/add', function(req, res){
 router.get('/editar/:id', function(req, res) {
   var id = req.params.id;
 
-  connection.query('SELECT * FROM PRODUTOS WHERE id = $1',[id] function(err, result) {
-    if (err){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('SELECT * FROM PRODUTOS WHERE id = $1',[id] function(err, result) 
+      done();
+      if (err){
       console.log(err);
     }
 
@@ -100,7 +101,9 @@ router.post('/editarProdutos', isLoggedIn,function(req,res){
                  tipo: req.body.tipo,
                  marca: req.body.marca}
 
- connection.query('UPDATE PRODUTOS SET $1 WHERE id = $2', [retorno, idproduto], function(err, result) {
+pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('UPDATE PRODUTOS SET $1 WHERE id = $2', [retorno, idproduto], function(err, result) {
+    done();
     if (err){
       console.log(err);
     }
@@ -112,7 +115,9 @@ router.get('/delete/:id',function(req,res){
     
     var id = req.params.id;
     
-  connection.query("DELETE FROM PRODUTOS WHERE id = $1",[id],function(err){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query("DELETE FROM PRODUTOS WHERE id = $1",[id],function(err){
+      done();
         if(err){
             console.log(err);
             res.send('Erro ao Deletar Usuario do Banco de Dados')
