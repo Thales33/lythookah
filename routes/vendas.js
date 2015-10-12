a0f256b58fc7e1d99fad8d09b42db0a33e4205aa
Thales <tpsthales@gmail.com>
2015-10-12 14:59:41 -0300

    venda

--- routes/vendas.js
+++ routes/vendas.js
@@ -37,7 +37,7 @@ router.get('/listvendas', function(req, res) {
 
 router.get('/novaVenda', function(req,res){
   pg.connect(process.env.DATABASE_URL, function(err, client, done){
-    client.query('SELECT e.idprodutos as idprodutos, p.descricao as descricao, p.precovenda as precovenda FROM produtos p inner join estoque e on (p.idprodutos = e.idprodutos) group by e.idprodutos, p.descricao, p.precovenda', function(err, result) { 
+    client.query('SELECT e.idprodutos as idprodutos, p.descricao as descricao, p.precovenda as precovenda FROM produtos p inner join estoque e on (p.idprodutos = e.idprodutos) group by e.idprodutos, p.descricao, p.precovenda;', function(err, result) { 
       done();
     if (err){
       console.log(err);
